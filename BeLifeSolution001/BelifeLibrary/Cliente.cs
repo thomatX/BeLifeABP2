a10f66d20ae13ddd5df5f7180a1b720aa1061857
thomatX <tweisskapp@gmail.com>
2018-05-09 23:14:44 -0300

    Development release 1.0
    Creada biblioteca de entidades y capa de negocio con sus respectivas verificaciones.

---
+++ BeLifeSolution001/BelifeLibrary/Cliente.cs
@@ -0,0 +1,100 @@
+﻿using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BelifeLibrary
+{
+    /// <summary>
+    /// Crea una nueva instancia de la clase 'Cliente'
+    /// Valida sus parametros a través de la clase de negocio
+    /// <seealso cref="Negocio.Configuracion"/>
+    /// </summary>
+    public class Cliente
+    {
+        /// <summary>
+        /// Cliente.Rut; [Devuelve el valor del atributo de la instancia Cliente]
+        /// Cliente.Rut = value; [Inserta un nuevo valor al atributo de la instancia Cliente]
+        /// Verifica el largo máximo para el rut del cliente (Valor máximo aceptado en el modelo de datos)
+        /// </summary>
+        public string Rut {
+            get { return Rut; }
+            set
+            {
+                /*Si el valor no excede el máximo de caracteres permitidos, inserta el nuevo valor a la propiedad*/
+                if (Negocio.Configuracion.ValidarRut(value))
+                    Rut = value;
+                else
+                    /*Si el valor insertado es mayor que el máximo de caracteres permitidos, envía una excepción*/
+                    throw new Exception("Error! El Rut del cliente no puede exceder los " + Negocio.Configuracion.MAXRUT + " caracteres");
+            }
+
+        }
+
+        /// <summary>
+        /// Cliente.Nombres; [Devuelve el valor del atributo de la instancia Cliente]
+        /// Cliente.Nombres = value; [Inserta un nuevo valor al atributo de la instancia Cliente]
+        /// Verifica el largo máximo para el Nombre del cliente (Valor máximo aceptado en el modelo de datos)
+        /// </summary>
+        public string Nombres
+        {
+            get { return Nombres; }
+            set
+            {
+                /*Si el valor no excede el máximo de caracteres permitidos, inserta el nuevo valor a la propiedad*/
+                if (Negocio.Configuracion.ValidarNombre(value))
+                    Nombres = value;
+                else
+                    /*Si el valor insertado es mayor que el máximo de caracteres permitidos, envía una excepción*/
+                    throw new Exception("Error! El Nombre del cliente no puede exceder los " + Negocio.Configuracion.MAXNOMBRE + " caracteres");
+            }
+
+        }
+
+        /// <summary>
+        /// Cliente.Apellidos; [Devuelve el valor del atributo de la instancia Cliente]
+        /// Cliente.Apellidos = value; [Inserta un nuevo valor al atributo de la instancia Cliente]
+        /// Verifica el largo máximo para el Apellido del cliente (Valor máximo aceptado en el modelo de datos)
+        /// </summary>
+        public string Apellidos
+        {
+            get { return Apellidos; }
+            set
+            {
+                /*Si el valor no excede el máximo de caracteres permitidos, inserta el nuevo valor a la propiedad*/
+                if (Negocio.Configuracion.ValidarApellido(value))
+                    Apellidos = value;
+                else
+                    /*Si el valor insertado es mayor que el máximo de caracteres permitidos, envía una excepción*/
+                    throw new Exception("Error! El Apellido del cliente no puede exceder los " + Negocio.Configuracion.MAXNOMBRE + " caracteres");
+            }
+
+        }
+
+        /// <summary>
+        /// Cliente.FechaNacimiento; [Devuelve el valor del atributo de la instancia Cliente]
+        /// Cliente.FechaNacimiento = value; [Inserta un nuevo valor al atributo de la instancia Cliente]
+        /// Verifica si el cliente cumple la edad requerida (Valor minimo aceptado en el modelo de datos)
+        /// </summary>
+        public DateTime FechaNacimiento
+        {
+            get { return FechaNacimiento; }
+            set
+            {
+                /*Si el valor no excede el máximo de caracteres permitidos, inserta el nuevo valor a la propiedad*/
+                if (Negocio.Configuracion.ValidarFechaNacimiento(value))
+                    FechaNacimiento = value;
+                else
+                    /*Si el valor insertado es mayor que el máximo de caracteres permitidos, envía una excepción*/
+                    throw new Exception("Error! El cliente no puede tener menos de "+Negocio.Configuracion.MINEDAD+" años.");
+            }
+
+        }
+
+
+
+    }
+}
---
+++ BeLifeSolution001/Negocio/Configuracion.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    /// <summary>
+    /// Almacena los valores máximos para cada verificación del modelo de entidades.
+    /// </summary>
+    public class Configuracion
+    {
+        /* ------------------------------------------------------------------------
+         *                                CLIENTES
+         *           Toda la configuración y validaciones de la clase Cliente
+           ---------------------------------------------------------------------  */
+        public static int MAXRUT = 10;
+        public static int MAXNOMBRE = 20;
+        public static int MAXAPELLIDO = 20;
+        public static int MINEDAD = 18;
+
+        // ------------------ Métodos validaciones Cliente ----------------------
+        /// <summary>
+        /// Metodo para validar nuevo rut del cliente
+        /// </summary>
+        /// <param name="value"></param>
+        /// Value: Nuevo rut cliente.
+        /// <returns></returns>
+        public static bool ValidarRut(string value) {
+            bool validated = false;
+            if (value.Length < MAXRUT) {
+                validated = true;
+            }
+            return validated;
+        }
+
+        /// <summary>
+        /// Metodo para validar nuevo Nombre del cliente
+        /// </summary>
+        /// <param name="value"></param>
+        /// Value: Nuevo Nombre cliente.
+        /// <returns></returns>
+        public static bool ValidarNombre(string value)
+        {
+            bool validated = false;
+            if (value.Length < MAXNOMBRE)
+            {
+                validated = true;
+            }
+            return validated;
+        }
+
+        /// <summary>
+        /// Metodo para validar nuevo Apellido del cliente
+        /// </summary>
+        /// <param name="value"></param>
+        /// Value: Nuevo Apellido cliente.
+        /// <returns></returns>
+        public static bool ValidarApellido(string value)
+        {
+            bool validated = false;
+            if (value.Length < MAXAPELLIDO)
+            {
+                validated = true;
+            }
+            return validated;
+        }
+
+        /// <summary>
+        /// Metodo para validar nueva fecha de nacimiento del cliente
+        /// </summary>
+        /// <param name="value"></param>
+        /// Value: Nueva Fecha de nacimiento del cliente.
+        /// <returns></returns>
+        public static bool ValidarFechaNacimiento(DateTime value) {
+            bool validated = false;
+            DateTime today = DateTime.Today;
+            int age = today.Year - value.Year;
+            if (age >= MINEDAD)
+            {
+                validated = true;
+            }
+            return validated;
+        }
+
+
+        /* ------------------------------------------------------------------------
+         *                                CONTRATOS
+         *        Toda la configuración y validaciones de la clase Contratos
+           ---------------------------------------------------------------------  */
+        public static int MAXNUMCONTRATO = 14;
+        public static int MAXCODIGOPLAN = 5;
+        public static int MAXIDPLAN = 5;
+        public static int MAXPOLIZAACTUAL = 15;
+
+        // -------------------- Métodos validaciones Contrato ------------------------
+        /// <summary>
+        /// Metodo para validar nuevo Numero de Contrato
+        /// </summary>
+        /// <param name="value"></param>
+        /// Value: Nuevo Numero Contrato.
+        /// <returns></returns>
+        public static bool ValidarNumContrato(string value)
+        {
+            bool validated = false;
+            if (value.Length < MAXNUMCONTRATO)
+            {
+                validated = true;
+            }
+            return validated;
+        }
+
+        /// <summary>
+        /// Metodo para validar nuevo CodigoPlan
+        /// </summary>
+        /// <param name="value"></param>
+        /// Value: Nuevo Codigo Plan.
+        /// <returns></returns>
+        public static bool ValidarCodigoPlan(string value)
+        {
+            bool validated = false;
+            if (value.Length < MAXCODIGOPLAN)
+            {
+                validated = true;
+            }
+            return validated;
+        }
+
+        /// <summary>
+        /// Metodo para validar nuevo ID Plan
+        /// </summary>
+        /// <param name="value"></param>
+        /// Value: Nuevo ID Plan.
+        /// <returns></returns>
+        public static bool ValidarIdPlan(string value)
+        {
+            bool validated = false;
+            if (value.Length < MAXIDPLAN)
+            {
+                validated = true;
+            }
+            return validated;
+        }
+
+        /// <summary>
+        /// Metodo para validar nueva Poliza Actual
+        /// </summary>
+        /// <param name="value"></param>
+        /// Value: Nueva Poliza Actual.
+        /// <returns></returns>
+        public static bool ValidarPolizaActual(string value)
+        {
+            bool validated = false;
+            if (value.Length < MAXPOLIZAACTUAL)
+            {
+                validated = true;
+            }
+            return validated;
+        }
+
+        /* ------------------------------------------------------------------------
+         *                                SEXO
+         *           Toda la configuración y validaciones de la clase Sexo
+           ---------------------------------------------------------------------  */
+        public static int MAXDESCSEXO = 10;
+
+        // -------------------- Métodos validaciones Sexo ------------------------
+        /// <summary>
+        /// Metodo para validar nuevo Descripcion del sexo
+        /// </summary>
+        /// <param name="value"></param>
+        /// Value: Nuevo Descripcion Sexo.
+        /// <returns></returns>
+        public static bool ValidarDescSexo(string value)
+        {
+            bool validated = false;
+            if (value.Length < MAXDESCSEXO)
+            {
+                validated = true;
+            }
+            return validated;
+        }
+
+
+
+        /* ------------------------------------------------------------------------
+         *                                ESTADO CIVIL
+         *        Toda la configuración y validaciones de la clase Estado Civil
+           ---------------------------------------------------------------------  */
+        public static int MAXDESCESTCIVIL = 15;
+
+        // -------------------- Métodos validaciones Estado Civil -----------------
+        /// <summary>
+        /// Metodo para validar nuevo Descripcion del Estado Civil
+        /// </summary>
+        /// <param name="value"></param>
+        /// Value: Nuevo Descripcion Estado Civil.
+        /// <returns></returns>
+        public static bool ValidarDescEstadoCivil(string value)
+        {
+            bool validated = false;
+            if (value.Length < MAXDESCESTCIVIL)
+            {
+                validated = true;
+            }
+            return validated;
+        }
+
+    }
+}
